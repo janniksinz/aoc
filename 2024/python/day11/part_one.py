c83024df2f33e635ee6cd6e365724deec94d7f9d
janniksinz <j.sinz@gmx.de>
2025-04-09 23:34:14 +0200

    feat: day11 part_one

--- 2024/python/day11/part_one.py
+++ 2024/python/day11/part_one.py
@@ -1,9 +1,22 @@
 import sys
 
 with open(sys.argv[1], 'r') as f:
-    grid = [list(map(int, line.strip())) for line in f.readlines()]
+    stones = list(map(int, f.read().strip().split(" ")))
 
-rows = len(grid)
-cols = len(grid[0])
+for _ in range(25):
+    new_stones = []
+    for stone in stones:
+        if stone == 0:
+            new_stones.append(1)
+        elif len(str(stone)) % 2 == 0:
+            stone_len = len(str(stone))
+            left = str(stone)[:stone_len//2]
+            right = str(stone)[stone_len//2:]
+            new_stones.append(int(left))
+            new_stones.append(int(right))
+        else:
+            new_stones.append(stone*2024)
 
-print(part1)
+        stones = new_stones
+
+print(len(stones))
